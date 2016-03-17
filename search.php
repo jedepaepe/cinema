1cf5cab55de44c25e7471b4622b116a892c1a190
jedepaepe <jedepaepe@PC8404.domepfc.net>
2016-03-17 19:45:25 +0100

    Set focus on input text + modic

--- search.php
+++ search.php
@@ -2,29 +2,38 @@
     <head>
         <meta charset="UTF-8">
         <title>films</title>
+        <link href="css/cinema.css" rel="stylesheet" type="text/css"/>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
     </head>
     <body> 
         <?php include './header.php'; ?>
-        <input id="titre" type="search" name="titre" placeholder="titre">
-        <input id="search" type="submit">
+        <input id="titre" type="search" name="titre" placeholder="titre" onkeyup="search(value)">
         <div id="result"></div>
     </body>
     <?php include './footer.php'; ?>
     <script>
-        $("#search").click(function() {
-            var value = $("#titre").val();
+        function search(value) {
+            /*if(arguments.length == 0) {
+                var value = $("#titre").val();
+            } else {
+                value = arguments[0];
+            }*/
+            if(value === undefined) {
+                var value = $("#titre").val();
+            }
             $.ajax(
                     {
                         url: "search_result.php?titre=" + value,
                         success: function(result) {
                             $("#result").html(result);
                         },
-                        error: function() {
+                        error: function() { 
                             $("#result").html("<h1 style='color: red'>ERROR</h1>");
                         }
                     }
             );
-        });
+        };
+        search();
+        $( "#titre" ).focus();
     </script>
 </html>
