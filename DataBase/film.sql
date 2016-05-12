db8364cd91f54da39dc51706caa8720ec645f2c4
jedepaepe <jedepaepe@PC8404.domepfc.net>
2016-05-12 18:44:40 +0200

    Ajouter "use" pour faciliter

--- DataBase/film.sql
+++ DataBase/film.sql
@@ -20,6 +20,7 @@ SET time_zone = "+00:00";
 -- Contenu de la table `film`
 --
 
+USE cinema;
 INSERT INTO `film` (`id`, `titre`, `annee`, `description`) VALUES
 (1, 'Le tout nouveau testament', 2015, 'Dieu existe. Il habite à Bruxelles. Il est odieux avec sa femme et sa fille. On a beaucoup parlé de son fils, mais très peu de sa fille. Sa fille c’est moi. Je m’appelle Ea et j’ai dix ans. Pour me venger j’ai balancé par SMS les dates de décès de tout le monde…'),
 (2, 'Mustang', 2015, 'C''est le début de l''été.\r\nDans un village reculé de Turquie, Lale et ses quatre sœurs rentrent de l’école en jouant avec des garçons et déclenchent un scandale aux conséquences inattendues.\r\nLa maison familiale se transforme progressivement en prison, les cours de pratiques ménagères remplacent l’école et les mariages commencent à s’arranger.\r\nLes cinq sœurs, animées par un même désir de liberté, détournent les limites qui leur sont imposées.'),
