42334024d2af2d0a1638f522341cf21a77fc9ae8
jedepaepe <jedepaepe@PC8404.domepfc.net>
2016-05-12 20:09:18 +0200

    Grid system & responsive menu + test page

--- header.php
+++ header.php
@@ -2,16 +2,23 @@
     <nav class="navbar navbar-inverse">
         <div class="container-fluid">
             <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mynavbar-content">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
                 <a class="navbar-brand" href="#">WebSiteName</a>
             </div>
-            <ul class="nav navbar-nav">
-                <li><a class="active" href="home.php">Accueil</a></li>
-                <li><a href="films.php">Films</a></li>
-                <li><a href="forum.php">Forum</a></li>
-                <li><a href="inquiry.php">Inquiry</a></li>
-                <li><a href="manage.php">Manage</a></li>
-                <li><a href="users.php">Users</a></li>
-            </ul>
+            <div class="collapse navbar-collapse" id="mynavbar-content">
+                <ul class="nav navbar-nav">
+                    <li><a class="active" href="home.php">Accueil</a></li>
+                    <li><a href="films.php">Films</a></li>
+                    <li><a href="forum.php">Forum</a></li>
+                    <li><a href="inquiry.php">Inquiry</a></li>
+                    <li><a href="manage.php">Manage</a></li>
+                    <li><a href="users.php">Users</a></li>
+                </ul>
+            </div>
         </div>
     </nav>
 </header>
